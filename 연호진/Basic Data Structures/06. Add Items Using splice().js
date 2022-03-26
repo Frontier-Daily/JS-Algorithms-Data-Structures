2c272d1d10a877bc5fa76a14efd350a6a7c39f1e
yeonlisa <tkzw4869@gmail.com>
2022-03-26 17:18:37 +0900

    Add Items Using splice()

--- 연호진/Basic Data Structures/06. Add Items Using splice().js
+++ 연호진/Basic Data Structures/06. Add Items Using splice().js
@@ -1,6 +1,6 @@
 function htmlColorNames(arr) {
     arr.splice(0,2,"DarkSalmon", "BlanchedAlmond");
     return arr;
-  }
-  
-  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
